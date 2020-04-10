98f3928c2247442b2da532a050ac41f7a5e6f8d3
mccauliflower <mccauliflower@protonmail.com>
2020-04-10 13:05:40 -0400

    weather app

--- server.js
+++ server.js
@@ -6,36 +6,33 @@ const axios = require('axios')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
-app.get('/',  (req, res) => { 
-  res.render('index', {weather: null, condition: null, error: null })
+app.get('/',  (req, res) => {
+  res.render('index', { weather: null, conditionIconUrl: '', error: null })
 })
 const apiKey = 'dae10b9ea136edea38f673138e3f83a1';
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  
-  axios.get(url)
-  .then( r => {
-      let body = r.data
-      if(body.main == undefined){
-        console.log("undefined")
-        res.render('index', {weather: null, condition: null,  error: 'Error, please try again'});
-      } else {
-        console.log("found", body.main)
-        condition = body.weather[0]['main'];
-        let weatherText = `It's ${body.main.temp} degrees in ${body.name}!`;
-        res.render('index', {weather: weatherText, condition,  error: null});
-      }
-    })
-    .catch(err => {
-      console.log('caught', err)
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    })
+  let weatherText, iconUrl, group, id
+
+  const r = await axios.get(url)
+  try{
+    let body = r.data
+    if(body.main == undefined){
+      res.render('index', {weather: null, conditionIconUrl: '',  error: 'Error, please try again'});
+    } else {
+      id = body.weather[0]['id']
+      console.log('Current Weather', body.weather[0]['main'])
+      weatherText = `It's ${body.main.temp} degrees in ${body.name}!`;
+      iconUrl = `http://openweathermap.org/img/wn/${body.weather[0]['icon']}@2x.png`
+      return res.render('index', {weather: weatherText, conditionIconUrl: iconUrl,  error: null});
+    }
+  }catch(err){
+    res.render('index', {weather: null, conditionIconUrl: '', error: 'Error, please try again'});
+  }
 })
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
-
-// <img src="<%= conditionIconUrl %>" alt="" class="logo">
